4a2c1e4942e7a37b086df161bbd21fd5216f5608
rihana19 <144229707+rihana19@users.noreply.github.com>
2023-09-22 22:25:40 +0530

    Add files via upload

--- main (1).py
+++ main (1).py
@@ -1,30 +1,13 @@
-class BankAccount:
-  def __init__(self,acno,acname,acbal=0.0):
-    self.__acno = acno
-    self.__acname=acname
-    self.__acbal=acbal
+def linearSearchProduct(productList,targetProduct):
+  indices = []
+  for index, product in enumerate(productList):
+    if product == targetProduct:
+      indices.append(index)
+  return indices
+# Example usage:
+products = ["shoes","boot","loafer","shoes","sandal","shoes"]
+target = "shoes"
+target2 = 'apple'
+result = linearSearchProduct(products,target)
+print(result)
 
-  def deposit(self,amt):
-    if amt > 0:
-      self.__acbal +=amt
-      print("Deposited Rs.{}. Your Current Balance is : Rs.{}".format(amt,self.__acbal))
-    else:
-      print("Invalid Data")
-
-  def withdraw(self,amt):
-    if amt > 0 and amt <= self.__acbal:
-      self.__acbal -= amt
-      print("Withdrew Rs.{}. YourCurrent Balance is Rs.{}".format(amt,self.__acbal))
-    else:
-      print("Invalid Transactions")
-
-  def display(self):
-    a=float(self.__acbal)
-    b=int(self.__acno)
-    print("Hai {} Balance for your Account No. {} is Rs.{}".format(self.__acname,b,a))
-
-
-acc = BankAccount(1234,"rihana",2000)
-acc.display()
-acc.deposit(500)
-acc.withdraw(1000)
